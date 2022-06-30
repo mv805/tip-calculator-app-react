927f6f90538e5e78c07eb0a87ba417538c58bf09
Matt Villa <matthewvilla805@gmail.com>
2022-06-30 15:58:00 -0700

    Field validation completed

--- src/components/NumberInput.js
+++ src/components/NumberInput.js
@@ -3,19 +3,41 @@ import styles from './NumberInput.module.css';
 
 const NumberInput = (props) => {
 
-    const [fieldValue, setFieldValue] = useState('0');
+    const [fieldInvalid, setFieldInvalid] = useState(false);
+
+    const inputHandler = (e) => {
+
+        console.log(e.target.value);
+
+        if (isNaN(e.target.value) ||
+            Number(e.target.value) < props.minValue ||
+            e.target.value === '') {
+            setFieldInvalid(true);
+            return;
+        }
+
+        setFieldInvalid(false);
+
+        props.onFieldUpdate(e.target.value);
+    };
+
+    const cleanupField = () => {
+
+    };
 
     return (
         <div className={ styles.input }>
             <label htmlFor={ props.field }>{ props.field }</label>
-            <div>
+            <div className={ fieldInvalid ? styles['invalid-field'] : '' }>
                 { props.symbol }
                 <input
-                    defaultValue={ fieldValue }
+                    defaultValue={ props.initialValue }
+                    onChange={ inputHandler }
                     name={ props.field }
                     id={ props.field }
                     type="number"
-                    min="0"/>
+                    min="0"
+                />
             </div>
         </div>
     );
--- src/components/TipCalculator.js
+++ src/components/TipCalculator.js
@@ -1,17 +1,44 @@
 import NumberInput from './NumberInput';
 import styles from './TipCalculator.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { faUser, faDollarSign } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const TipCalculator = () => {
 
-    const moneySymbol = <FontAwesomeIcon icon={ solid('dollar-sign') } />;
-    const peopleSymbol = <FontAwesomeIcon icon={ solid('user') } />;
+    const moneySymbol = <FontAwesomeIcon icon={ faDollarSign } />;
+    const peopleSymbol = <FontAwesomeIcon icon={ faUser } />;
+
+    const [billAmount, setBillAmount] = useState('0');
+    const [numOfPeople, setNumOfPeople] = useState('1');
+    // const [billField, setbillField] = useState('');
+    // const [peopleField, setPeopleField] = useState('');
+
+    const setBill = (price) => {
+        setBillAmount(Number(price).toFixed(2));
+    };
+
+    const setAmountOfPeople = (people) => {
+        setNumOfPeople(Math.round(Number(people)));
+    };
+
+    console.log('Bill: ', billAmount);
+    console.log('Num of People: ', numOfPeople);
 
     return (
         <div className={ styles.panel }>
-            <NumberInput field="Bill" symbol={ moneySymbol } />
-            <NumberInput field="Number of People" symbol={ peopleSymbol } wholenumber />
+            <NumberInput
+                field="Bill"
+                symbol={ moneySymbol }
+                onFieldUpdate={ setBill }
+                minValue={ 0 }
+                initialValue={ 0 } />
+            <NumberInput
+                field="Number of People"
+                symbol={ peopleSymbol }
+                onFieldUpdate={ setAmountOfPeople }
+                minValue={ 1 }
+                initialValue={ 1 } />
         </div>
     );
 };
